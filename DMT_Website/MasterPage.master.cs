94d892570e6eaecc64ab46ff92bb36a8879db7b0
organyx <aleksandr.beliun@gmail.com>
2015-11-04 14:09:03 +0100

    Adding localisation

--- DMT_Website/MasterPage.master.cs
+++ DMT_Website/MasterPage.master.cs
@@ -10,5 +10,38 @@ public partial class MasterPage : System.Web.UI.MasterPage
     protected void Page_Load(object sender, EventArgs e)
     {
 
+        //The dropdownlist should show the selected language,
+        //which we can read from the Culture cookie
+        if (!IsPostBack)
+        {
+            HttpCookie cultureCookie = Request.Cookies["Culture"];
+
+            string cultureCode;
+
+            if (cultureCookie != null)
+            {
+                cultureCode = cultureCookie.Value;
+            }
+            else
+            {
+                cultureCode = null;
+            }
+
+            if (!string.IsNullOrEmpty(cultureCode))
+            {
+                DropDownList1.SelectedValue = cultureCode;
+            }
+
+        }
+    }
+
+    //when a new value in the dropdownlist is selected, write it to the Culture cookie
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        //Save Current Culture in Cookie- will be used in InitializeCulture in BasePage
+        Response.Cookies.Add(new HttpCookie("Culture", DropDownList1.SelectedValue));
+        //Reload
+        Response.Redirect(Request.Url.AbsolutePath);
+
     }
 }
